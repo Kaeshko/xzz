64cb14c8cfa1129ab2543c2c039ab203ef97c116
Kaeshko <thekaeshko@gmail.com>
2026-09-09 03:20:56 +0900

    Update script.lua

--- script.lua
+++ script.lua
@@ -175,7 +175,7 @@ local function singleHit()
 			root.AssemblyLinearVelocity = Vector3.zero
 			root.AssemblyAngularVelocity = Vector3.zero
 		end
-		task.wait(0.02)
+		task.wait(0.04)
 		iscanbe = true
 	end
 end
